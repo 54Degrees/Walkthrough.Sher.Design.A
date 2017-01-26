1391a9ca18d07283e18b90d7ac335a90dfe275ac
Ellen Corcoran-Walsh <54degreesdesign@gmail.com>
2017-01-26 14:51:11 +0000

    Walkthrough Sher Design (Kitchen A)

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sher-design-kitchen-a1",
+      "name": "Sher Design Kitchen A1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.5694041897697844,
+        "pitch": 0.1481561074890152,
+        "fov": 1.4753221924046438
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3802099081777595,
+          "pitch": 0.33376519747308997,
+          "rotation": 0,
+          "target": "2-sher-design-kitchen-a3"
+        },
+        {
+          "yaw": 2.70742369697186,
+          "pitch": 0.6890153599486837,
+          "rotation": 0,
+          "target": "1-sher-design-kitchen-a2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sher-design-kitchen-a2",
+      "name": "Sher Design Kitchen A2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.529007320869261,
+        "pitch": 0.33017433259403006,
+        "fov": 1.3135790439744743
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1780053365915375,
+          "pitch": 0.7774666520885063,
+          "rotation": 0,
+          "target": "0-sher-design-kitchen-a1"
+        },
+        {
+          "yaw": -0.495707167030476,
+          "pitch": 0.7622922921197492,
+          "rotation": 0,
+          "target": "2-sher-design-kitchen-a3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sher-design-kitchen-a3",
+      "name": "Sher Design Kitchen A3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.2521916076367141,
+        "pitch": 0.37233819284145575,
+        "fov": 1.475324005429518
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5050995773705154,
+          "pitch": 0.2932947035228448,
+          "rotation": 0,
+          "target": "0-sher-design-kitchen-a1"
+        },
+        {
+          "yaw": 0.11126969067961312,
+          "pitch": 0.642001700148537,
+          "rotation": 0,
+          "target": "1-sher-design-kitchen-a2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Sher Design Kitchens A",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
